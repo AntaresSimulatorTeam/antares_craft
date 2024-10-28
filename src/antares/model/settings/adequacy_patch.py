815cfa912a31684b66e442afc5bb0e5bf167b1b9
Sigurd Borge <sigurd.borge_externe@rte-france.com>
2024-10-28 17:57:34 +0100

    Added default values and ini_fields to AdequacyPatchParameters

--- src/antares/model/settings/adequacy_patch.py
+++ src/antares/model/settings/adequacy_patch.py
@@ -12,7 +12,7 @@
 
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 from pydantic.alias_generators import to_camel
 
 from antares.tools.all_optional_meta import all_optional_model
@@ -24,12 +24,14 @@ class PriceTakingOrder(Enum):
 
 
 class DefaultAdequacyPatchParameters(BaseModel, populate_by_name=True, alias_generator=to_camel):
+    model_config = ConfigDict(use_enum_values=True)
+
     # version 830
     enable_adequacy_patch: bool = False
     ntc_from_physical_areas_out_to_physical_areas_in_adequacy_patch: bool = True
     ntc_between_physical_areas_out_adequacy_patch: bool = True
     # version 850
-    price_taking_order: PriceTakingOrder = PriceTakingOrder.DENS
+    price_taking_order: PriceTakingOrder = Field(default=PriceTakingOrder.DENS, validate_default=True)
     include_hurdle_cost_csr: bool = False
     check_csr_cost_function: bool = False
     enable_first_step: bool = False
@@ -46,4 +48,21 @@ class AdequacyPatchParameters(DefaultAdequacyPatchParameters):
 class AdequacyPatchParametersLocal(DefaultAdequacyPatchParameters):
     @property
     def ini_fields(self) -> dict:
-        return {}
+        return {
+            "adequacy patch": {
+                "include-adq-patch": str(self.enable_adequacy_patch).lower(),
+                "set-to-null-ntc-from-physical-out-to-physical-in-for-first-step": str(
+                    self.ntc_from_physical_areas_out_to_physical_areas_in_adequacy_patch
+                ).lower(),
+                "set-to-null-ntc-between-physical-out-for-first-step": str(
+                    self.ntc_between_physical_areas_out_adequacy_patch
+                ).lower(),
+                "enable-first-step": str(self.enable_first_step).lower(),
+                "price-taking-order": self.price_taking_order,
+                "include-hurdle-cost-csr": str(self.include_hurdle_cost_csr).lower(),
+                "check-csr-cost-function": str(self.check_csr_cost_function).lower(),
+                "threshold-initiate-curtailment-sharing-rule": f"{self.threshold_initiate_curtailment_sharing_rule:.6f}",
+                "threshold-display-local-matching-rule-violations": f"{self.threshold_display_local_matching_rule_violations:.6f}",
+                "threshold-csr-variable-bounds-relaxation": f"{self.threshold_csr_variable_bounds_relaxation}",
+            }
+        }
