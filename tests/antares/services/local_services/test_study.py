02a6e0c48d203ad686bb0fdcb50b9007f4f66d82
Sigurd Borge <sigurd.borge_externe@rte-france.com>
2024-11-22 12:06:52 +0100

    Changing the signature of create_study_local to take a string to indicate the directory to create in instead of providing a "LocalConfiguration" object which isn't needed to provide by the user.

--- tests/antares/services/local_services/test_study.py
+++ tests/antares/services/local_services/test_study.py
@@ -14,7 +14,6 @@ import pytest
 import logging
 import os
 import time
-import typing as t
 
 from configparser import ConfigParser
 from pathlib import Path
@@ -83,6 +82,7 @@ from antares.model.settings.study_settings import DefaultStudySettings, StudySet
 from antares.model.settings.thematic_trimming import DefaultThematicTrimmingParameters, ThematicTrimmingParametersLocal
 from antares.model.study import create_study_local
 from antares.tools.ini_tool import IniFileTypes
+from antares.tools.time_series_tool import TimeSeriesFileType
 
 
 class TestCreateStudy:
