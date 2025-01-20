d2f4be066b7b63e9bbdf661684592bbeb8f5b27f
killian-scalian <killian.pirot@scalian.com>
2025-01-20 15:28:30 +0100

    fix

--- tests/antares/services/local_services/test_area.py
+++ tests/antares/services/local_services/test_area.py
@@ -1127,20 +1127,37 @@ class TestReadArea:
         local_study_object = read_study_local(study_path)
         optimization_path = study_path / "input" / "thermal" / "areas.ini"
 
-        antares_content = f"""[unserverdenergycost]
-fr = 1.800000
-it = 1.000000
+        antares_content = """[unserverdenergycost]
+at = 10000.000000
+be = 10000.000000
+ch = 10000.000000
+de = 10000.000000
+es = 10000.000000
+fr = 10000.000000
+gb = 10000.000000
+ie = 10000.000000
+it = 10000.000000
+lu = 10000.000000
 
 [spilledenergycost]
-fr = 1.200000
-it = 1.000000
+at = 10.000000
+be = 10.000000
+ch = 10.000000
+de = 10.000000
+es = 10.000000
+fr = 10.000000
+gb = 10.000000
+ie = 10.000000
+it = 10.000000
+lu = 10.000000
     """
         with open(optimization_path, "w", encoding="utf-8") as antares_file:
             antares_file.write(antares_content)
         local_study_object.read_areas()
         area_fr = local_study_object.get_areas()["fr"]
-        assert area_fr.properties.energy_cost_spilled == 1.2
-        assert area_fr.properties.energy_cost_unsupplied == 1.8
+        assert area_fr.properties.energy_cost_unsupplied == 10000
+        assert area_fr.properties.energy_cost_spilled == 10
+        
 
 
 def _write_file(_file_path, _time_series) -> None:
