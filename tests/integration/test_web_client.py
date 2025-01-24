13ef31dea9b1a8df953def2697b067efdc20bb4d
wahadameh <mehdi.wahada@rte-france.com>
2025-01-24 13:36:59 +0100

    feat(api): included integration tests and correcting minor errors

--- tests/integration/test_web_client.py
+++ tests/integration/test_web_client.py
@@ -452,6 +452,15 @@ class TestWebClient:
         study.delete_link(link_de_fr)
         assert link_de_fr.id not in study.get_links()
 
+        # tests uploading thermal and renewable matrices
+        thermal_fr_matrix = pd.DataFrame(data=[[0]])
+        renewable_fr_matrix = pd.DataFrame(data=[[0]])
+        thermal_fr.update_thermal_matrix(thermal_fr_matrix)
+        renewable_fr.update_renewable_matrix(renewable_fr_matrix)
+
+        actual_thermal_matrix = thermal_fr.get_series_matrix()
+        actual_thermal_matrix.equals(thermal_fr_matrix)
+
         # tests thermal cluster deletion
         area_be.delete_thermal_cluster(thermal_be)
         assert area_be.get_thermals() == {}
@@ -463,15 +472,6 @@ class TestWebClient:
         # tests short term storage deletion
         area_fr.delete_st_storage(battery_fr)
         assert battery_fr.id not in study.get_areas().get(area_be.id).get_st_storages()
-
-        # tests uploading thermal and renewable matrices
-        thermal_fr_matrix = pd.DataFrame(data=[[0]])
-        renewable_fr_matrix = pd.DataFrame(data=[[0]])
-        thermal_fr.update_thermal_matrix(thermal_fr_matrix)
-        renewable_fr.update_renewable_matrix(renewable_fr_matrix)
-
-        actual_thermal_matrix = thermal_fr.get_series_matrix()
-        actual_thermal_matrix.equals(thermal_fr_matrix)
 
         # tests area deletion error
         with pytest.raises(
