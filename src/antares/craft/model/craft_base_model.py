8e65c3f0cdbbe9674aba056836649537b798bc01
wahadameh <mehdi.wahada@rte-france.com>
2025-01-28 16:13:50 +0100

    changed pydantic version

--- src/antares/craft/model/craft_base_model.py
+++ src/antares/craft/model/craft_base_model.py
@@ -16,9 +16,3 @@ from pydantic import BaseModel, ConfigDict
 
 class CraftBaseModel(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    def model_dump(self, **kwargs) -> dict[str, Any]:
-        return super().model_dump(serialize_as_any=True, **kwargs)
-
-    def model_dump_json(self, **kwargs) -> str:
-        return super().model_dump_json(serialize_as_any=True, **kwargs)
