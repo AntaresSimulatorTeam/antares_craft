11ee5f2c0804645a2a1bf0b77dbca10294020e27
wahadameh <mehdi.wahada@rte-france.com>
2025-01-28 16:11:22 +0100

    changed pydantic version

--- src/antares/craft/model/craft_base_model.py
+++ src/antares/craft/model/craft_base_model.py
@@ -9,9 +9,16 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from typing import Any
 
 from pydantic import BaseModel, ConfigDict
 
 
 class CraftBaseModel(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def model_dump(self, **kwargs) -> dict[str, Any]:
+        return super().model_dump(serialize_as_any=True, **kwargs)
+
+    def model_dump_json(self, **kwargs) -> str:
+        return super().model_dump_json(serialize_as_any=True, **kwargs)
