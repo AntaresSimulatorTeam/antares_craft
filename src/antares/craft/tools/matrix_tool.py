6111cbe52b5e91f50d599a4ea92912980f3655a7
killian-scalian <killian.pirot@scalian.com>
2025-02-25 12:03:40 +0100

    Fixes

--- src/antares/craft/tools/matrix_tool.py
+++ src/antares/craft/tools/matrix_tool.py
@@ -65,6 +65,13 @@ def read_timeseries(
     return _time_series
 
 
-def write_timeseries(study_path: Path, series: pd.DataFrame, ts_file_type: TimeSeriesFileType, area_id: str) -> None:
-    file_path = study_path.joinpath(ts_file_type.value.format(area_id=area_id))
+def write_timeseries(study_path: Path, series: pd.DataFrame, ts_file_type: TimeSeriesFileType, area_id: str, cluster_id: Optional[str] = None, second_area_id: Optional[str] = None) -> None:
+    if second_area_id:
+        file_path = study_path.joinpath(ts_file_type.value.format(area_id=area_id, second_area_id=second_area_id))
+    elif cluster_id:
+        file_path = study_path.joinpath(ts_file_type.value.format(area_id=area_id, cluster_id=cluster_id))
+    else:
+        file_path = study_path.joinpath(ts_file_type.value.format(area_id=area_id))
+    if not file_path.parent.is_dir():
+        file_path.parent.mkdir(parents=True)
     series.to_csv(file_path, sep="\t", header=False, index=False, encoding="utf-8")
