35a58d1f0826f0bc3e1dc884a0d0613dd5c12061
belthlemar <martin.belthle@rte-france.com>
2025-03-03 16:48:30 +0100

    modify test

--- tests/antares/services/local_services/test_link.py
+++ tests/antares/services/local_services/test_link.py
@@ -9,16 +9,11 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-import pytest
 
-import re
 
 from pathlib import Path
 
-import pandas as pd
-
 from antares.craft import Study
-from antares.craft.exceptions.exceptions import MatrixFormatError
 from antares.craft.model.link import AssetType, LinkProperties, LinkPropertiesUpdate
 
 
@@ -35,39 +30,12 @@ class TestLink:
         assert new_properties == expected_properties
         assert link.properties == expected_properties
 
-    def test_matrices(self, tmp_path: Path, local_study_w_storage: Study) -> None:
+    def test_matrices(self, tmp_path: Path, local_study_w_links: Study) -> None:
         # Checks all matrices exist and are empty
-        storage = local_study_w_storage.get_areas()["fr"].get_st_storages()["sts_1"]
-        assert storage.get_pmax_injection().empty
-        assert storage.get_pmax_withdrawal().empty
-        assert storage.get_lower_rule_curve().empty
-        assert storage.get_upper_rule_curve().empty
-        assert storage.get_storage_inflows().empty
+        link = local_study_w_links.get_links()["at / fr"]
+        assert link.get_parameters().empty
+        assert link.get_capacity_direct().empty
+        assert link.get_capacity_indirect().empty
 
         # Replace matrices
-        matrix = pd.DataFrame(data=8760 * [[3]])
-
-        storage.update_pmax_injection(matrix)
-        assert storage.get_pmax_injection().equals(matrix)
-
-        storage.update_pmax_withdrawal(matrix)
-        assert storage.get_pmax_withdrawal().equals(matrix)
-
-        storage.update_lower_rule_curve(matrix)
-        assert storage.get_lower_rule_curve().equals(matrix)
-
-        storage.update_upper_rule_curve(matrix)
-        assert storage.get_upper_rule_curve().equals(matrix)
-
-        storage.update_storage_inflows(matrix)
-        assert storage.get_storage_inflows().equals(matrix)
-
-        # Try to update with wrongly formatted matrix
-        matrix = pd.DataFrame(data=[[1, 2, 3], [4, 5, 6]])
-        with pytest.raises(
-            MatrixFormatError,
-            match=re.escape(
-                "Wrong format for storage/fr/sts_1/pmax_injection matrix, expected shape is (8760, 1) and was : (2, 3)"
-            ),
-        ):
-            storage.update_pmax_injection(matrix)
+        # todo
