0d224cdc433117e8f0da691d29ca0427a6f51855
belthlemar <martin.belthle@rte-france.com>
2025-03-03 16:40:37 +0100

    fix

--- tests/antares/services/local_services/test_link.py
+++ tests/antares/services/local_services/test_link.py
@@ -19,21 +19,21 @@ import pandas as pd
 
 from antares.craft import Study
 from antares.craft.exceptions.exceptions import MatrixFormatError
-from antares.craft.model.st_storage import STStorageProperties, STStoragePropertiesUpdate
+from antares.craft.model.link import AssetType, LinkProperties, LinkPropertiesUpdate
 
 
 class TestLink:
-    def test_update_properties(self, tmp_path: Path, local_study_w_storage: Study) -> None:
+    def test_update_properties(self, tmp_path: Path, local_study_w_links: Study) -> None:
         # Checks values before update
-        storage = local_study_w_storage.get_areas()["fr"].get_st_storages()["sts_1"]
-        current_properties = STStorageProperties(efficiency=0.4, initial_level_optim=True)
-        assert storage.properties == current_properties
+        link = local_study_w_links.get_links()["at / fr"]
+        current_properties = LinkProperties(hurdles_cost=False, asset_type=AssetType.AC)
+        assert link.properties == current_properties
         # Updates properties
-        update_properties = STStoragePropertiesUpdate(efficiency=0.1, reservoir_capacity=1.2)
-        new_properties = storage.update_properties(update_properties)
-        expected_properties = STStorageProperties(efficiency=0.1, initial_level_optim=True, reservoir_capacity=1.2)
+        update_properties = LinkPropertiesUpdate(hurdles_cost=True, comments="new comment")
+        new_properties = link.update_properties(update_properties)
+        expected_properties = LinkProperties(hurdles_cost=True, asset_type=AssetType.AC, comments="new comment")
         assert new_properties == expected_properties
-        assert storage.properties == expected_properties
+        assert link.properties == expected_properties
 
     def test_matrices(self, tmp_path: Path, local_study_w_storage: Study) -> None:
         # Checks all matrices exist and are empty
