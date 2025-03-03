2e202c3ef2f57c0c443c37ba126fce5219d66470
belthlemar <martin.belthle@rte-france.com>
2025-03-03 17:18:34 +0100

    simplify test

--- tests/antares/services/local_services/test_study.py
+++ tests/antares/services/local_services/test_study.py
@@ -1382,7 +1382,7 @@ at%fr = 1%1
 
     def test_get_constraint_matrix(self, local_study):
         # Given
-        expected_time_series = pd.DataFrame(np.random.random([365 * 24, 1]))
+        expected_time_series = pd.DataFrame(np.random.randint(0, 100, [365 * 24, 1]))
         bc_name = "test time series"
         local_study.create_binding_constraint(
             name=bc_name,
@@ -1396,4 +1396,4 @@ at%fr = 1%1
         actual_time_series = local_study.get_binding_constraints()[bc_name].get_greater_term_matrix()
 
         # Then
-        assert actual_time_series.round(10).equals(expected_time_series.round(10))
+        assert actual_time_series.equals(expected_time_series)
