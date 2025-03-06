43da8faa1a5c395fd1dbe6d3f161a1132453134c
belthlemar <martin.belthle@rte-france.com>
2025-03-06 11:37:16 +0100

    start code

--- src/antares/craft/tools/matrix_tool.py
+++ src/antares/craft/tools/matrix_tool.py
@@ -18,6 +18,52 @@ import pandas as pd
 
 from antares.craft.tools.time_series_tool import TimeSeriesFileType
 
+DEFAULT_MATRIX_MAPPING = {
+    TimeSeriesFileType.BINDING_CONSTRAINT_EQUAL: "",
+    TimeSeriesFileType.BINDING_CONSTRAINT_GREATER: "",
+    TimeSeriesFileType.BINDING_CONSTRAINT_LESS: "",
+    TimeSeriesFileType.HYDRO_MAX_POWER: "",
+    TimeSeriesFileType.HYDRO_RESERVOIR: "",
+    TimeSeriesFileType.HYDRO_INFLOW_PATTERN: "",
+    TimeSeriesFileType.HYDRO_CREDITS_MODULATION: "",
+    TimeSeriesFileType.HYDRO_WATER_VALUES: "",
+    TimeSeriesFileType.HYDRO_ROR: "",
+    TimeSeriesFileType.HYDRO_MOD: "",
+    TimeSeriesFileType.HYDRO_MINGEN: "",
+    TimeSeriesFileType.HYDRO_ENERGY: "",
+    TimeSeriesFileType.LINKS_CAPACITIES_DIRECT: "",
+    TimeSeriesFileType.LINKS_CAPACITIES_INDIRECT: "",
+    TimeSeriesFileType.LINKS_PARAMETERS: "",
+    TimeSeriesFileType.LOAD: "",
+    TimeSeriesFileType.LOAD_CONVERSION: "",
+    TimeSeriesFileType.LOAD_DATA: "",
+    TimeSeriesFileType.LOAD_K: "",
+    TimeSeriesFileType.LOAD_TRANSLATION: "",
+    TimeSeriesFileType.MISC_GEN: "",
+    TimeSeriesFileType.RENEWABLE_DATA_SERIES: "",
+    TimeSeriesFileType.ST_STORAGE_PMAX_INJECTION: "",
+    TimeSeriesFileType.ST_STORAGE_PMAX_WITHDRAWAL: "",
+    TimeSeriesFileType.ST_STORAGE_INFLOWS: "",
+    TimeSeriesFileType.ST_STORAGE_LOWER_RULE_CURVE: "",
+    TimeSeriesFileType.ST_STORAGE_UPPER_RULE_CURVE: "",
+    TimeSeriesFileType.RESERVES: "",
+    TimeSeriesFileType.SOLAR: "",
+    TimeSeriesFileType.SOLAR_CONVERSION: "",
+    TimeSeriesFileType.SOLAR_DATA: "",
+    TimeSeriesFileType.SOLAR_K: "",
+    TimeSeriesFileType.SOLAR_TRANSLATION: "",
+    TimeSeriesFileType.THERMAL_SERIES: "",
+    TimeSeriesFileType.THERMAL_DATA: "",
+    TimeSeriesFileType.THERMAL_MODULATION: "",
+    TimeSeriesFileType.THERMAL_CO2: "",
+    TimeSeriesFileType.THERMAL_FUEL: "",
+    TimeSeriesFileType.WIND: "",
+    TimeSeriesFileType.WIND_CONVERSION: "",
+    TimeSeriesFileType.WIND_DATA: "",
+    TimeSeriesFileType.WIND_K: "",
+    TimeSeriesFileType.WIND_TRANSLATION: "",
+}
+
 
 def read_timeseries(
     ts_file_type: TimeSeriesFileType,
@@ -37,7 +83,7 @@ def read_timeseries(
     if os.path.getsize(file_path) != 0:
         _time_series = pd.read_csv(file_path, sep="\t", header=None)
     else:
-        _time_series = pd.DataFrame()
+        _time_series = pd.DataFrame(DEFAULT_MATRIX_MAPPING[ts_file_type])
 
     return _time_series
 
