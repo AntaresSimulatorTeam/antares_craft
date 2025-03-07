159767b099dab1e2e8aee6600e6a9072ebe5f8d2
mehdiwahada <mehdi.wahada@rte-france.com>
2025-03-07 14:35:57 +0100

    feat(api): introduce `update_multiple_areas_properties` method (#104)

--- src/antares/craft/model/study.py
+++ src/antares/craft/model/study.py
@@ -20,7 +20,7 @@ from antares.craft import APIconf
 from antares.craft.exceptions.exceptions import (
     LinkCreationError,
 )
-from antares.craft.model.area import Area, AreaProperties, AreaUi
+from antares.craft.model.area import Area, AreaProperties, AreaPropertiesUpdate, AreaUi
 from antares.craft.model.binding_constraint import (
     BindingConstraint,
     BindingConstraintProperties,
@@ -96,6 +96,11 @@ class Study:
         areas = list(self._areas.values())
         areas.sort(key=lambda area: area.id)
         return areas
+
+    def update_multiple_areas(self, new_properties: Dict[str, AreaPropertiesUpdate]) -> None:
+        new_areas_props = self._area_service.update_multiple_areas(new_properties)
+        for area_prop in new_areas_props:
+            self._areas[area_prop]._properties = new_areas_props[area_prop]
 
     def read_links(self) -> list[Link]:
         link_list = self._link_service.read_links()
