e24d433edafe58ff356e184fad9eeb0a7c852467
belthlemar <martin.belthle@rte-france.com>
2025-05-19 15:47:07 +0200

    ALL TESTS OK

--- src/antares/craft/service/api_services/services/output.py
+++ src/antares/craft/service/api_services/services/output.py
@@ -10,6 +10,7 @@
 #
 # This file is part of the Antares project.
 from io import StringIO
+from pathlib import Path
 
 import pandas as pd
 
@@ -33,13 +34,31 @@ class OutputApiService(BaseOutputService):
 
     @override
     def get_matrix(self, output_id: str, file_path: str, frequency: Frequency) -> pd.DataFrame:
-        full_path = f"output/{output_id}/economy/{file_path}"
+        api_path = self._convert_path_for_web(file_path)
+        full_path = f"output/{output_id}/economy/{api_path}"
 
         raw_url = f"{self._base_url}/studies/{self.study_id}/raw/original-file?path={full_path}"
         response = self._wrapper.get(raw_url)
         data = StringIO(response.text)
 
         return read_output_matrix(data, frequency)
+
+    @staticmethod
+    def _convert_path_for_web(file_path: str) -> str:
+        # Note: AntaresWeb being completely stupid, it changed the path for links so we have to handle this case here ...
+        parts = Path(file_path).parts
+        if parts[0] == "mc-all" and parts[1] == "links":
+            index = 2
+        elif parts[0] == "mc-ind" and parts[2] == "links":
+            index = 3
+        else:
+            return file_path
+
+        local_formatting = parts[index].split(" - ")
+        api_formatting = f"{local_formatting[0]}/{local_formatting[1]}"
+        api_parts = list(parts)
+        api_parts[index] = api_formatting
+        return "/".join(api_parts)
 
     @override
     def aggregate_values(
