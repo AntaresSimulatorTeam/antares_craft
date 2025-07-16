940b99d1bd52a47d1e55269580cc4edc3c582f06
belthlemar <martin.belthle@rte-france.com>
2025-07-16 15:06:24 +0200

    continue test

--- tests/antares/services/local_services/test_xpansion.py
+++ tests/antares/services/local_services/test_xpansion.py
@@ -9,20 +9,31 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
+from pathlib import Path
 
-
-from antares.craft import Study
+from antares.craft import Study, read_study_local
 from antares.craft.model.xpansion.settings import XpansionSettings
 from antares.craft.model.xpansion.xpansion_configuration import XpansionConfiguration
 
 
 class TestXpansion:
     def test_create_configuration(self, local_study_w_links: Study) -> None:
+        # Asserts at first Xpansion is None.
         assert local_study_w_links.xpansion is None
+        # Reading a study without an Xpansion configuration works. It returns None as an ` xpansion ` attribute.
+        study_path = Path(local_study_w_links.path)
+        study = read_study_local(study_path)
+        assert study.xpansion is None
 
+        # Create Xpansion configuration.
         xpansion = local_study_w_links.create_xpansion_configuration()
         assert isinstance(xpansion, XpansionConfiguration)
         assert xpansion.settings == XpansionSettings()
         assert xpansion.get_candidates() == {}
         assert xpansion.get_constraints() == {}
         assert xpansion.sensitivity is None
+
+        # Asserts the reading works.
+        study_path = Path(local_study_w_links.path)
+        study = read_study_local(study_path)
+        assert study.xpansion is not None
