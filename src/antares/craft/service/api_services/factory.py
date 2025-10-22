b95f3be5ea635adcc421bc4a4623ff278352e512
belthlemar <martin.belthle@rte-france.com>
2025-10-22 16:28:00 +0200

    still areas and xpansion to handle

--- src/antares/craft/service/api_services/factory.py
+++ src/antares/craft/service/api_services/factory.py
@@ -12,7 +12,7 @@
 import io
 
 from pathlib import Path, PurePath
-from typing import Optional
+from typing import Any, Optional
 
 from antares.craft import (
     AreaProperties,
@@ -68,6 +68,9 @@ from antares.craft.service.api_services.services.study import StudyApiService
 from antares.craft.service.api_services.services.thermal import ThermalApiService
 from antares.craft.service.api_services.services.xpansion import XpansionAPIService
 from antares.craft.service.base_services import (
+    BaseBindingConstraintService,
+    BaseLinkService,
+    BaseOutputService,
     StudyServices,
 )
 
@@ -163,69 +166,46 @@ def import_study_api(api_config: APIconf, study_path: Path, destination_path: Op
         raise StudyImportError(study_path.name, e.message) from e
 
 
+def create_variant_api(api_config: APIconf, study_id: str, variant_name: str) -> "Study":
+    """
+    Creates a variant from a study_id
+    Args:
+        api_config: API configuration
+        study_id: The id of the study to create a variant of
+        variant_name: the name of the new variant
+    Returns: The variant in the form of a Study object
+    """
+    services = create_api_services(api_config, study_id)
+    return services.study_service.create_variant(variant_name)
+
+
+
 def read_study_api(api_config: APIconf, study_id: str) -> "Study":
     session = api_config.set_up_api_conf()
     wrapper = RequestWrapper(session)
     base_url = f"{api_config.get_host()}/api/v1"
-    json_api = wrapper.get(f"{base_url}/studies/{study_id}/craft").json()
+    json_api = wrapper.get(f"{base_url}/studies/{study_id}/data").json()
 
     services = create_api_services(api_config, study_id)
 
-    ####### STUDY #######
-    study_name = json_api.pop("name")
-    study_version = json_api.pop("version")
-    path = json_api.pop("path")
-    pure_path = PurePath(path) if path else PurePath(".")
+    # Metadata
+    study = _read_study_metadata(json_api, services)
 
-    study = Study(study_name, study_version, services, pure_path)
+    # Links
+    study._links = _read_links(json_api, services.link_service)
 
-    ####### LINKS #######
-    links = {}
-    links_api = json_api.pop("links")
-    for props in links_api.values():
-        area_from = props.pop("area1")
-        area_to = props.pop("area2")
+    # Binding constraints
+    study._binding_constraints = _read_binding_constraints(json_api, services.bc_service)
 
-        api_response = LinkPropertiesAndUiAPI.model_validate(props)
-        link_properties = api_response.to_properties_user_model()
-        link_ui = api_response.to_ui_user_model()
-
-        link = Link(area_from, area_to, services.link_service, link_properties, link_ui)
-        links[link.id] = link
-
-    study._links = links
-
-    ####### BINDING CONSTRAINTS #######
-    constraints = {}
-    bcs_api = json_api.pop("bcs")
-    for bc_api in bcs_api:
-        constraint_name = bc_api.pop("name")
-        del bc_api["id"]
-
-        api_terms = bc_api.pop("terms")
-        api_properties = BindingConstraintPropertiesAPI.model_validate(bc_api)
-        bc_properties = api_properties.to_user_model()
-
-        terms: list[ConstraintTerm] = []
-        for api_term in api_terms:
-            term_data = ConstraintTermData.from_dict(api_term["data"])
-            terms.append(ConstraintTerm(weight=api_term["weight"], offset=api_term["offset"], data=term_data))
-
-        bc = BindingConstraint(constraint_name, services.bc_service, bc_properties, terms)
-        constraints[bc.id] = bc
-
-    study._binding_constraints = constraints
-
-    ####### OUTPUTS #######
-    outputs = {}
-    api_outputs = json_api.pop("outputs")
-    for output_id, output_props in api_outputs.items():
-        output = Output(output_service=services.output_service, name=output_id, archived=output_props["archived"])
-        outputs[output.name] = output
-
-    study._outputs = outputs
+    # Outputs
+    study._outputs = _read_outputs(json_api, services.output_service)
 
     ###### XPANSION ########
+    if "xpansion" not in json_api:
+        study._xpansion_configuration = None
+    else:
+        xpansion_api = json_api.pop("xpansion")
+
     xp_config = json_api.pop("xp_settings")
     if not xp_config:
         study._xpansion_configuration = None
@@ -253,28 +233,8 @@ def read_study_api(api_config: APIconf, study_id: str) -> "Study":
             constraints=xp_constraints,
         )
 
-    ###### SETTINGS ########
-    nb_ts_thermal = json_api.pop("ts_config")["thermal"]["number"]
-    general_parameters = parse_general_parameters_api(json_api.pop("general_config"), nb_ts_thermal)
-    thematic_trimming_parameters = parse_thematic_trimming_api(json_api.pop("thematic_config"))
-    optimization_parameters = parse_optimization_parameters_api(json_api.pop("opt_config"))
-    adequacy_patch_parameters = parse_adequacy_patch_parameters_api(json_api.pop("adequacy_config"))
-    advanced_parameters, seed_parameters = parse_advanced_and_seed_parameters_api(json_api.pop("adv_config"))
-
-    user_playlist = {}
-    api_playlist = json_api.pop("playlist").get("years", {})
-    for key, value in api_playlist.items():
-        user_playlist[int(key)] = PlaylistParameters(**value)
-
-    study._settings = StudySettings(
-        general_parameters=general_parameters,
-        optimization_parameters=optimization_parameters,
-        seed_parameters=seed_parameters,
-        advanced_parameters=advanced_parameters,
-        adequacy_patch_parameters=adequacy_patch_parameters,
-        playlist_parameters=user_playlist,
-        thematic_trimming_parameters=thematic_trimming_parameters,
-    )
+    # Settings
+    study._settings = _read_settings(json_api)
 
     ###### AREAS ########
 
@@ -366,14 +326,81 @@ def read_study_api(api_config: APIconf, study_id: str) -> "Study":
     return study
 
 
-def create_variant_api(api_config: APIconf, study_id: str, variant_name: str) -> "Study":
-    """
-    Creates a variant from a study_id
-    Args:
-        api_config: API configuration
-        study_id: The id of the study to create a variant of
-        variant_name: the name of the new variant
-    Returns: The variant in the form of a Study object
-    """
-    services = create_api_services(api_config, study_id)
-    return services.study_service.create_variant(variant_name)
+def _read_links(body: dict[str, Any], link_service: BaseLinkService) -> dict[str, Link]:
+    links = {}
+    links_api = body["links"]
+    for link in links_api:
+        area_from = link.pop("area1")
+        area_to = link.pop("area2")
+
+        api_response = LinkPropertiesAndUiAPI.model_validate(link)
+        link_properties = api_response.to_properties_user_model()
+        link_ui = api_response.to_ui_user_model()
+
+        link = Link(area_from, area_to, link_service, link_properties, link_ui)
+        links[link.id] = link
+
+    return links
+
+def _read_binding_constraints(body: dict[str, Any], bc_service: BaseBindingConstraintService) -> dict[str, BindingConstraint]:
+    constraints = {}
+    bcs_api = body["binding_constraints"]
+    for bc_api in bcs_api:
+        constraint_name = bc_api.pop("name")
+        del bc_api["id"]
+
+        api_terms = bc_api.pop("terms")
+        api_properties = BindingConstraintPropertiesAPI.model_validate(bc_api)
+        bc_properties = api_properties.to_user_model()
+
+        terms: list[ConstraintTerm] = []
+        for api_term in api_terms:
+            term_data = ConstraintTermData.from_dict(api_term["data"])
+            terms.append(ConstraintTerm(weight=api_term["weight"], offset=api_term["offset"], data=term_data))
+
+        bc = BindingConstraint(constraint_name, bc_service, bc_properties, terms)
+        constraints[bc.id] = bc
+
+    return constraints
+
+def _read_settings(body: dict[str, Any]) -> StudySettings:
+    settings_api = body["settings"]
+    nb_ts_thermal = settings_api["timeSeries"]["thermal"]["number"]
+    general_parameters = parse_general_parameters_api(settings_api["general"], nb_ts_thermal)
+    thematic_trimming_parameters = parse_thematic_trimming_api(settings_api["thematicTrimming"])
+    optimization_parameters = parse_optimization_parameters_api(settings_api["optimization"])
+    adequacy_patch_parameters = parse_adequacy_patch_parameters_api(settings_api["adequacyPatch"])
+    advanced_parameters, seed_parameters = parse_advanced_and_seed_parameters_api(settings_api["advancedParameters"])
+
+    user_playlist = {}
+    api_playlist = settings_api["playlist"].get("years", {})
+    for key, value in api_playlist.items():
+        user_playlist[int(key)] = PlaylistParameters(**value)
+
+    return StudySettings(
+        general_parameters=general_parameters,
+        optimization_parameters=optimization_parameters,
+        seed_parameters=seed_parameters,
+        advanced_parameters=advanced_parameters,
+        adequacy_patch_parameters=adequacy_patch_parameters,
+        playlist_parameters=user_playlist,
+        thematic_trimming_parameters=thematic_trimming_parameters,
+    )
+
+def _read_study_metadata(body: dict[str, Any], services: StudyServices) -> Study:
+    study_metadata = body["metadata"]
+    study_name = study_metadata["name"]
+    study_version = study_metadata["version"]
+    folder = study_metadata.get("folder")
+    pure_path = PurePath(folder) if folder else PurePath(".")
+
+    return Study(study_name, study_version, services, pure_path)
+
+def _read_outputs(body: dict[str, Any], output_service: BaseOutputService) -> dict[str, Output]:
+    outputs = {}
+    api_outputs = body["outputs"]
+    for api_otp in api_outputs:
+        output = Output(output_service=output_service, name=api_otp["name"], archived=api_otp["archived"])
+        outputs[output.name] = output
+
+    return outputs
