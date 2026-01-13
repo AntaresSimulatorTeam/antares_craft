6260a313d0904e9e7157710e9487096260df1840
MartinBelthle <martin.belthle@rte-france.com>
2026-01-13 19:37:55 +0100

    fix(local, run): allow simulations to run on Windows (#330)

---
+++ scripts/find_release.sh
@@ -0,0 +1,34 @@
+VERSION=$1
+ARTEFACT_NAME=$2
+
+echo "Finding release ID..."
+
+release_id=$(curl -s \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$REPO/releases | \
+  jq --arg ver "$VERSION" '.[] | select(.name == $ver) | .id')
+
+if [ -z "$release_id" ]; then
+  echo "Release not found!"
+  exit 1
+fi
+echo "Release ID: $release_id"
+
+echo "Finding AntaresWeb asset ID..."
+asset_id=$(curl -s \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$REPO/releases/$release_id/assets | \
+  jq --arg release "$ARTEFACT_NAME" \
+   '.[] | select(.name == ($release + ".zip")) | .id')
+
+if [ -z "$asset_id" ]; then
+  echo "Asset not found!"
+  exit 1
+fi
+echo "Asset ID: $asset_id"
+
+echo "Downloading asset..."
+curl -L -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/octet-stream" \
+  https://api.github.com/repos/$REPO/releases/assets/$asset_id \
+  -o "$ARTEFACT_NAME".zip
